f5eb6afe8f43fd629fac5a834444f995f0e45870
Zigmas Petrauskas <zigmas.1@gmail.com>
2024-12-12 09:04:33 +0200

    Pridėta main ir footer su responsive design

--- templates/src/App.jsx
+++ templates/src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./index.scss";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 };
