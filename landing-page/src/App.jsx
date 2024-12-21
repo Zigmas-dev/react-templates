25773f0f8bf84f3a9208b1c6112a74e715a6de60
Zigmas Petrauskas <zigmas.1@gmail.com>
2024-12-21 15:56:21 +0200

    Sukurtas landinpage jsx scss, pakoreguotas app index.scss

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,13 @@
+import LandingPage from "./components/LandingPage";
+import "./index.scss";
+
+const App = () => {
+ 
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+};
+       
+export default App;
