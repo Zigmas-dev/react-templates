7681258783287490c750c5de729d8d841b5795c0
Zigmas Petrauskas <zigmas.1@gmail.com>
2025-02-02 20:18:33 +0200

    Pridėjau footeri

--- website/frontend/src/App.jsx
+++ website/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./index.scss";
 
 const App = () => {
   return (
     <>
      <Header />
-      
+
+     <Footer /> 
     </>
   );
 };
---
+++ website/frontend/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import { FaReact, FaJs, FaNode, FaGithub, FaFacebook } from "react-icons/fa";
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="icon-container">
+        <div className="icon" color="#61DBFB">
+          <FaReact color="#61DBFB" />
+        </div>
+        <div className="icon">
+          <FaJs color="#F7DF1E" />
+        </div>
+        <div className="icon">
+          <FaNode color="#83CD29" />
+        </div>
+        <div className="icon">
+          <FaGithub color="#181717" />
+        </div>
+        <div className="icon">
+          <FaFacebook color="#1877F2" />
+        </div>
+      </div>   
+      <p>&copy; 2024 zigmaswebdev.lt. Visos teisės ginamos.</p>
+    </div>
+  );
+};
+
+export default Footer;
