652aedca17ccd3146736af2786d2b4e9bc18ad0e
Zigmas Petrauskas <zigmas.1@gmail.com>
2025-02-03 08:19:29 +0200

    sdfgsdgsdgsg

--- templates/src/components/Footer.jsx
+++ templates/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import "./footer.scss";
 
 const Footer = () => {
    return (
-     <footer className="footer">
-       <p>&copy; 2024 Visos teisės ginamos.</p>
+     <footer className="footer"> 
+      <p>&copy; 2024 Visos teisės ginamos.</p>
        <div className="social-links">
         <a href="https://github.com/" target="_blank" rel="noopener noreferrer">GitHub</a>
         <a href="https://facebook.com" target="_blank" rel="noopener noreferre">Facebook</a>
