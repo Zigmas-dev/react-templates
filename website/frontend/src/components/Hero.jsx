9ae7c1176e3b853a82dedd4d2bab748ae6923cc7
Zigmas Petrauskas <zigmas.1@gmail.com>
2025-02-08 11:03:12 +0200

    pridėtas hero į home

---
+++ website/frontend/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import "./hero.scss";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+     <h1>zigmaswebdev.lt</h1>
+     <p>Jūsų idėja - mano kodas.</p>
+     <p>Paverskime viziją realybę!</p>
+    </div> 
+  );
+};
+
+export default Hero;
--- website/frontend/src/pages/Home.jsx
+++ website/frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-
 import Links from "../components/Links";
+import Hero from "../components/Hero";
 import "./home.scss";
 
 const Home = () => {
    return (
       <div className="home-container">
        <Links />
+       <Hero />
       </div>
    );
 };
