53d3ea533b294096c2ce79ffa05b681bd96d9480
Zigmas Petrauskas <zigmas.1@gmail.com>
2025-03-01 11:42:43 +0200

    pridėta cors bcrypt mysql2 jsonwebtoken

--- invoice-app/backend/index.js
+++ invoice-app/backend/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`🚀 Serveris veikia ant ${port} porto`);
