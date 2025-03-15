49af5b75c444fc2a530aeef2bd2a52c8d80d3243
Zigmas Petrauskas <zigmas.1@gmail.com>
2025-03-15 17:42:10 +0200

    pridėta grižimo funkcija

--- eshop/frontend/src/e-components/Navbar.jsx
+++ eshop/frontend/src/e-components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <div className={styles.logo}>E-SHOP</div>
       <div className={`${styles['nav-links']} ${isMenuOpen ? styles.open : ''}`}>
-        <Link to="/main-page" className={styles.link}>Pagrindinis</Link>
+        <Link to="/" className={styles.link}>Atgal į svetainę</Link>
         <Link to="/products" className={styles.link}>Produktai</Link>
         <Link to="/faq" className={styles.link}>D.U.K</Link>
         <Link to="/cart" className={styles.link}>Krepšelis</Link>
