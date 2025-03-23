5aabea5b5ce0ea1916b97fbea630a52833da47b4
Zigmas Petrauskas <zigmas.1@gmail.com>
2025-03-23 08:19:07 +0200

    koreguotas scss

--- eshop/frontend/src/e-pages/MainPage.jsx
+++ eshop/frontend/src/e-pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../e-components/NavBar";
+import Searchbar from "../e-components/Searchbar";
 import styles from "./MainPage.module.scss";
 
 const MainPage = () => {
   return (
     <div className={styles['main-page']}>
       <Navbar />
+      <Searchbar />
       <h1>E-parduotuvės Pagrindinis Puslapis</h1>
       {/* Čia galite pridėti kitą turinį */}
     </div>
