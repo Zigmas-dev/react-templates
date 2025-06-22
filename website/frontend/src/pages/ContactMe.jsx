f245e650ad7050496c4b6ba24ea0bfbeff915628
Zigmas-dev <zigmasdev@gmail.com>
2025-06-22 08:58:25 +0300

    asdsadfsd

--- website/frontend/src/pages/ContactMe.jsx
+++ website/frontend/src/pages/ContactMe.jsx
@@ -1,14 +1,17 @@
 import Image from "../components/Image";
-import ContactForm from "../components/ContactForm";
+// import ContactForm from "../components/ContactForm";
 import "./contactMe.scss";
 
 const ContactMe = () => {
-   return (
-     <div className="contact-container">
-       <Image />
-       <ContactForm />
-     </div>
-   );
+  return (
+    <div className="contact-container">
+      <Image />
+      <div className="contact-message">
+        <p>Susisiekti su manim galite info@zigmaswebdev.lt</p> 
+      </div>
+      {/* <ContactForm /> */}
+    </div>
+  );
 };
 
 export default ContactMe;
